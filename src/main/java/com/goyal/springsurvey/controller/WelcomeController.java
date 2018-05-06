2431dc4538f7738b196a99373c185ac66a16b3dd
Ayush Goyal <ayush987goyal@gmail.com>
2018-05-06 11:58:21 +0530

    Add basic configuration with spring

--- src/main/java/com/goyal/springsurvey/controller/WelcomeController.java
+++ src/main/java/com/goyal/springsurvey/controller/WelcomeController.java
@@ -1,9 +1,13 @@
 package com.goyal.springsurvey.controller;
 
+import com.goyal.springsurvey.configuration.BasicConfiguration;
 import com.goyal.springsurvey.service.WelcomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class WelcomeController {
@@ -11,8 +15,21 @@ public class WelcomeController {
     @Autowired
     private WelcomeService welcomeService;
 
+    @Autowired
+    private BasicConfiguration basicConfiguration;
+
     @RequestMapping("/welcome")
     public String welcome() {
         return welcomeService.getWelcomeMessage();
+    }
+
+    @RequestMapping("/dynamic-config")
+    public Map dynamiConfiguration() {
+        Map map = new HashMap();
+        map.put("message", basicConfiguration.getMessage());
+        map.put("number", basicConfiguration.getNumber());
+        map.put("value", basicConfiguration.isValue());
+
+        return map;
     }
 }
